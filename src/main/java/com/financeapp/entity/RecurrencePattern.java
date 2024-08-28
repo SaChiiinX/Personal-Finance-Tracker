caf0bbb86a9c82665b7bba58ac868649ab134388
sanjae <sachinxsan@gmail.com>
2024-08-27 20:34:51 -0400

    added transaction & recurrence type enums

---
+++ src/main/java/com/financeapp/entity/RecurrencePattern.java
@@ -0,0 +1,5 @@
+package com.financeapp.entity;
+
+public enum RecurrencePattern {
+    DAILY, WEEKLY, MONTHLY, BI_MONTHLY, QUARTELY, SEMI_YEARLY, YEARLY
+}
---
+++ src/main/java/com/financeapp/entity/TransactionType.java
@@ -0,0 +1,5 @@
+package com.financeapp.entity;
+
+public enum TransactionType {
+    INCOME, EXPENSE
+}
