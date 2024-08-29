18de51a22eb8c935c5ed4db630296506ff4babd3
sanjae <sachinxsan@gmail.com>
2024-08-29 10:53:25 -0400

    Properly intialized transaction repo

--- src/main/java/com/financeapp/repository/TransactionRepository.java
+++ src/main/java/com/financeapp/repository/TransactionRepository.java
@@ -1,5 +1,11 @@
 package com.financeapp.repository;
 
-public class TransactionRepository {
-    
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.financeapp.entity.Transaction;
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
